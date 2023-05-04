505bea79fb168016af268dcd528f456328b955ed
lucasrpatten <lucasrpattendev@gmail.com>
2023-05-03 20:03:27 -0600

    Web UI Scaffolding

--- web/main.js
+++ web/main.js
@@ -1,51 +1,63 @@
 let matches = [];
+let rejections = [];
 
 const inputElement = (userInput) => {
-    return (
-        [
-            '<div class="input-element">',
-            '<p>',
-            userInput,
-            '</p>',
-            '<button class="delete-button">Delete</button>',
-        ].join("")
-    )
-}
+    return `
+    <div class="input-element">
+      <span>${userInput}</span>
+      <button class="delete-button">Delete</button>
+    </div>
+  `;
+};
 
-const updateMatches = () => {
-}
-
-const addMatch = () => {
-    var userInput = $("#matching-input").val();
-    if (!userInput.length > 0) {
+const addToList = (list, input, inputId) => {
+    if (input.trim().length === 0) {
         return;
     }
-    matches.push(userInput);
-    $('#matches').append(inputElement(userInput));
-    $(this).val('');
-}
+    if (list.includes(input)) {
+        alert(`Matching Texts Cannot Contain Duplicates`);
+        return;
+    }
+    list.push(input);
+    $(`#${inputId}`).append(inputElement(input));
+    $(`#${inputId} input`).val("");
+};
 
-const deleteMatch = (element) => {
-    matches = matches.filter(function (item) {
-        return item != $(element).parent('.input-element').text().replace("Delete", "")
-    });
-    $(element).parent('.input-element').remove();
-}
 
-$(document).ready(function () {
-    $("#matching-input").on("keyup", function (event) {
-        if (event.keyCode === 13) {
+const deleteFromList = (list, element) => {
+    const text = $(element).siblings("span").text();
+    list = list.filter(item => item !== text);
+    $(element).parent().remove();
+};
+
+$(function () {
+    $("#match-input").on("keyup", event => {
+        if (event.key === "Enter") {
             event.preventDefault();
-            addMatch();
+            addToList(matches, $(event.target).val(), "matches");
         }
     });
 
-    $("#matches").on('click', '.delete-button', function () {
-        deleteMatch(this);
+    $("#rejection-input").on("keyup", event => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            addToList(rejections, $(event.target).val(), "rejections");
+        }
     });
 
-    $("#add-match").click(function (event) {
+    $("#matches, #rejections").on("click", ".delete-button", event => {
+        const parent = $(event.target).closest(".input-element").parent();
+        if (parent.is("#matches")) {
+            deleteFromList(matches, event.target);
+        } else {
+            deleteFromList(rejections, event.target);
+        }
+    });
+
+    $("#add-match, #add-rejection").click(event => {
         event.preventDefault();
-        addMatch();
+        const inputId = $(event.target).data("input-id");
+        const inputVal = $(`#${inputId} input`).val();
+        addToList(inputId === "matches" ? matches : rejections, inputVal, inputId);
     });
 });
