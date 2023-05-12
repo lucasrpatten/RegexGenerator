929cccaaee558716ed6b2fd062f021943d5633c3
lucasrpatten <lucasrpattendev@gmail.com>
2023-05-11 20:57:41 -0600

    added data getter

--- model/add_data.py
+++ model/add_data.py
@@ -1,5 +1,35 @@
 from database_management import Database
 import re
+from bs4 import BeautifulSoup
+import requests
+import time
+
+class WebScraper():
+    @classmethod
+    def collect(cls):
+        d = DatabaseManager("./model/data.db", "patterns")
+        for i in range(20, 5000):
+            url = f"https://www.regexlib.com/REDetails.aspx?regexp_id={i}"
+            r = requests.get(url)
+            data = r.text
+            soup = BeautifulSoup(data, "html.parser")
+            expression = ""
+            matches = []
+            rejections = []
+            try:
+                expression = soup.find(id="ctl00_ContentPlaceHolder1_ExpressionLabel").getText()
+                matches = soup.find(id="ctl00_ContentPlaceHolder1_MatchesLabel").getText().split(" | ")
+                rejections = soup.find(id="ctl00_ContentPlaceHolder1_NonMatchesLabel").getText().split(" | ")
+                print(i)
+                print(expression, matches, rejections)
+                if re.fullmatch(expression, matches) is None:
+                    raise AttributeError()
+                if re.fullmatch(expression, rejections) is not None:
+                    raise AttributeError()
+                d.addData(matches, rejections, expression)
+            except (AttributeError, TypeError, re.error):
+                pass
+            time.sleep(0.1)
 
 class DatabaseManager(Database):
     def __init__(self, database_path="./data.db", table="patterns"):
@@ -25,3 +55,5 @@ class DatabaseManager(Database):
                     f"'{i}' should not match pattern '{output_pattern}'")
 
         self.add_row(accepted_texts, rejected_texts, output_pattern)
+
+WebScraper.collect()
